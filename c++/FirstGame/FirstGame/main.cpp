f9ba42761d1c2c05a1ceb6494cd3ae2a86b9ec1e
mehdi khocha <Freego@MacBook-Pro-de-mehdi.local>
2015-08-19 17:05:46 +0200

    petit test en c++

---
+++ c++/FirstGame/FirstGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstGame
+//
+//  Created by Freego on 19/08/2015.
+//  Copyright (c) 2015 Freego. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout<<"hello world!!!"<<std::endl<<"cette fois ca marche mais, c'est quoi std::?"<<std::endl;
+    return 0;
+}
